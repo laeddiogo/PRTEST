8f0d87ef48912f54628d7fc66f6bca465800f456
mina1 <mina1@M1AWS>
2022-01-28 15:41:03 +0000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> hosts = new Dictionary<int, string>();
+
+            hosts.Add(1,"api");
+            hosts.Add(2,"api");
+            hosts.Add(3,"api");
+            hosts.Add(4,"api");
+
+            bool p = hosts.Remove(8);
+            string t = hosts[1];
+            Console.WriteLine("t");
+            
+
+
+        }
+    }
+}
